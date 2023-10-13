b99bcdb0be6dc4e310ee6ed2920020152ee38a74
Griffin <griffinjohnston@gmail.com>
2023-10-13 19:21:33 -0400

    +global.d.ts removed "global" and import to turn it into a script vs module

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,52 +1,48 @@
-export {}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      bouncy: any
-      'bouncy-arc': any
-      cardio: any
-      'chaotic-orbit': any
-      'dot-pulse': any
-      'dot-spinner': any
-      'dot-stream': any
-      'dot-wave': any
-      grid: any
-      hatch: any
-      helix: any
-      hourglass: any
-      infinity: any
-      jelly: any
-      'jelly-stream': any
-      'jelly-triangle': any
-      leapfrog: any
-      'line-spinner': any
-      'line-wobble': any
-      metronome: any
-      miyagi: any
-      momentum: any
-      'newtons-cradle': any
-      orbit: any
-      ping: any
-      pinwheel: any
-      pulsar: any
-      quantum: any
-      reuleaux: any
-      ring: any
-      ring2: any
-      ripples: any
-      spiral: any
-      square: any
-      squircle: any
-      superballs: any
-      'tail-chase': any
-      tailspin: any
-      treadmill: any
-      trefoil: any
-      trio: any
-      waveform: any
-      wobble: any
-      zoomies: any
-    }
+declare namespace JSX {
+  interface IntrinsicElements {
+    bouncy: any
+    'bouncy-arc': any
+    cardio: any
+    'chaotic-orbit': any
+    'dot-pulse': any
+    'dot-spinner': any
+    'dot-stream': any
+    'dot-wave': any
+    grid: any
+    hatch: any
+    helix: any
+    hourglass: any
+    infinity: any
+    jelly: any
+    'jelly-stream': any
+    'jelly-triangle': any
+    leapfrog: any
+    'line-spinner': any
+    'line-wobble': any
+    metronome: any
+    miyagi: any
+    momentum: any
+    'newtons-cradle': any
+    orbit: any
+    ping: any
+    pinwheel: any
+    pulsar: any
+    quantum: any
+    reuleaux: any
+    ring: any
+    ring2: any
+    ripples: any
+    spiral: any
+    square: any
+    squircle: any
+    superballs: any
+    'tail-chase': any
+    tailspin: any
+    treadmill: any
+    trefoil: any
+    trio: any
+    waveform: any
+    wobble: any
+    zoomies: any
   }
 }
